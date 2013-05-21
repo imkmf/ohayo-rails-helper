a7f185c080c663a9678f7624667d368796262791
Kristian Freeman <kristian@kristianfreeman.com>
2013-05-21 16:59:04 -0700

    Update spec for foobar user

--- spec/moshimoshi_rails_helper_spec.rb
+++ spec/moshimoshi_rails_helper_spec.rb
@@ -6,7 +6,7 @@ describe Rails::MoshimoshiRailsHelper::Helper do
   end
 
   it "should return a greeting" do
-    email = "kristian@kristianfreeman.com"
-    @stub.moshimoshi_tag(email).should == "Foo"
+    email = "foo@bar.org"
+    @stub.moshimoshi_tag(email).should == "I'm foobar: I was used to test the API for this site and now I have to hang out by myself. Sad day. "
   end
 end
